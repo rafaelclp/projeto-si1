a053bf4e2ffa373353f286e64551fb009bd2fdb6
rafaelclp <rafael.clp@hotmail.com>
2014-03-01 19:23:01 -0300

    Fixed

--- app/model/Grade.java
+++ app/model/Grade.java
@@ -1,4 +1,4 @@
-package model;
+﻿package model;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,9 +37,73 @@ public class Grade {
 	// PURE FABRICATION: Tinha que haver um modo das disciplinas entrarem na grade,
 	//					resolvemos fazer por leitura de arquivo.
 	private void preencherDisciplinas() {
+disciplinas.add(new Disciplina("Programação I", 4, 4, 1, 1));
+		disciplinas.add(new Disciplina("Leitura e Prod. de Textos", 4, 2, 1, 2));
+		disciplinas.add(new Disciplina("Cálculo I", 4, 7, 1, 3));
+		disciplinas.add(new Disciplina("Algebra Vetorial", 4, 3, 1, 4));
+		disciplinas.add(new Disciplina("Int. à Computacação", 4, 5, 1, 5));
+		disciplinas.add(new Disciplina("Lab. de Programação I", 4, 4, 1, 6));
+		disciplinas.add(new Disciplina("Programação II", 4, 5, 2, 7));
+		disciplinas.add(new Disciplina("Lab. de Programação II", 4, 5, 2, 8));
+		disciplinas.add(new Disciplina("Matemática Discreta", 4, 5, 2, 9));
+		disciplinas.add(new Disciplina("Metodologia Científica", 4, 4, 2, 10));
+		disciplinas.add(new Disciplina("Teoria dos Grafos", 2, 3, 2, 11));
+		disciplinas.add(new Disciplina("Fund. de Física Clássica", 4, 8, 2, 12));
+		disciplinas.add(new Disciplina("Cálculo II", 4, 7, 2, 13));
+		disciplinas.add(new Disciplina("Estrutura de Dados e Algoritmos", 4, 7, 3, 14));
+		disciplinas.add(new Disciplina("Lab. de Estrutura de Dados e Algoritmos", 4, 7, 3, 15));
+		disciplinas.add(new Disciplina("Algebra Linear", 4, 9, 3, 16));
+		disciplinas.add(new Disciplina("Probabilidade e Est.", 4, 9, 3, 17));
+		disciplinas.add(new Disciplina("Teoria da Computação", 4, 6, 3, 18));
+		disciplinas.add(new Disciplina("Fund. de Física Moderna", 4, 7, 3, 19));
+		disciplinas.add(new Disciplina("Gerência da Informação", 4, 5, 3, 20));
+		disciplinas.add(new Disciplina("Métodos Estatísticos", 4, 5, 4, 21));
+		disciplinas.add(new Disciplina("Paradigmas de Linguagens de Programação", 2, 5, 4, 22));
+		disciplinas.add(new Disciplina("Org. e Arquitetura de Computadores I", 4, 5, 4, 23));
+		disciplinas.add(new Disciplina("Lab. de Org. e Arquitetura de Computadores", 4, 5, 4, 24));
+		disciplinas.add(new Disciplina("Lógica Matemática", 4, 5, 4, 25));
+		disciplinas.add(new Disciplina("Sistemas de Informação I", 4, 5, 4, 26));
+		disciplinas.add(new Disciplina("Engenharia de Software I", 4, 5, 4, 27));
+		disciplinas.add(new Disciplina("Análise e Técnicas de Algoritmos", 4, 9, 5, 28));
+		disciplinas.add(new Disciplina("Redes de Computadores", 4, 5, 5, 29));
+		disciplinas.add(new Disciplina("Compiladores", 4, 9, 5, 30));
+		disciplinas.add(new Disciplina("Banco de Dados I", 4, 5, 5, 31));
+		disciplinas.add(new Disciplina("Sistemas de Informação II", 4, 5, 5, 32));
+		disciplinas.add(new Disciplina("Laboratório de Engenharia de Software", 4, 5, 5, 33));
+		disciplinas.add(new Disciplina("Informática e Sociedade", 2, 5, 5, 34));
+		disciplinas.add(new Disciplina("Sistemas Operacionais", 4, 5, 6, 35));
+		disciplinas.add(new Disciplina("Interconexão de Redes de Computadores", 4, 5, 6, 36));
+		disciplinas.add(new Disciplina("Lab. de Interconexão de Redes de Computadores", 4, 5, 6, 37));
+		disciplinas.add(new Disciplina("Inteligência Artificial I", 4, 5, 6, 38));
+		disciplinas.add(new Disciplina("Banco de Dados II", 4, 5, 6, 39));
+		disciplinas.add(new Disciplina("Direito e Cidadania", 4, 5, 6, 40));
+		disciplinas.add(new Disciplina("Métodos e Software Númericos", 4, 5, 7, 41));
+		disciplinas.add(new Disciplina("Aval. de Desempenho de Sist. Discretos", 4, 5, 7, 42));
+		disciplinas.add(new Disciplina("Projeto em Computação I", 4, 5, 7, 43));
+		disciplinas.add(new Disciplina("Projeto em Computação II", 6, 5, 8, 44));
+		disciplinas.add(new Disciplina("Administração", 4, 5, 0, 45));
+		disciplinas.add(new Disciplina("TECC (Administração de SGBDs)", 4, 5, 0, 46));
+		disciplinas.add(new Disciplina("TECC (Computação e Música)", 4, 3, 0, 47));
+		disciplinas.add(new Disciplina("Criatividade", 4, 2, 0, 48));
+		disciplinas.add(new Disciplina("Ergonomia", 4, 3, 0, 49));
+		disciplinas.add(new Disciplina("Gestão da Qualidade", 4, 5, 0, 50));
+		disciplinas.add(new Disciplina("TECC (Inteligência Artificial II)", 4, 8, 0, 51));
+		disciplinas.add(new Disciplina("Marketing para Informática", 4, 5, 0, 52));
+		disciplinas.add(new Disciplina("Métodos Avançados de Programação", 4, 7, 0, 53));
+		disciplinas.add(new Disciplina("Organização e Arquitetura de Computadores II", 4, 8, 0, 54));
+		disciplinas.add(new Disciplina("Percepção da Forma", 4, 3, 0, 55));
+		disciplinas.add(new Disciplina("Prática Desportiva", 2, 1, 0, 56));
+		disciplinas.add(new Disciplina("Processo Decisório", 4, 5, 0, 57));
+		disciplinas.add(new Disciplina("Psicologia da Aprendizagem", 4, 3, 0, 58));
+		disciplinas.add(new Disciplina("Relações Humanas", 4, 3, 0, 59));
+		disciplinas.add(new Disciplina("Sociologia Industrial I", 4, 5, 0, 60));
+		disciplinas.add(new Disciplina(" Teoria e Prática da Cor", 4, 2, 0, 61));
+		disciplinas.add(new Disciplina("TECC (Algoritmos Avançados I)", 2, 9, 0, 62));
+		disciplinas.add(new Disciplina("TECC (Redes Ad Hoc sem fio)", 4, 5, 0, 63));
+		disciplinas.add(new Disciplina("TECC (Jogos digitais)", 4, 5, 0, 64));
 		// cria classe para leitura de dados apartir de arquivo.
-		LeitorArquivo loader = new LeitorArquivo();
-		disciplinas = loader.carregaDisciplinas();
+		//LeitorArquivo loader = new LeitorArquivo();
+		//disciplinas = loader.carregaDisciplinas();
 	}
 
 	/**
