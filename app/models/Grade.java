e6a9b2bb02147d5f36122c94431104fbf13ff9f2
rafaelclp <rafael.clp@hotmail.com>
2014-04-06 17:21:09 -0300

    Grande refatoramento realizado, javadoc reescrito (estava vergonhoso e cheio de erros)

--- app/models/Grade.java
+++ app/models/Grade.java
@@ -12,9 +12,9 @@ import javax.persistence.Transient;
 
 import play.db.ebean.Model;
 
-
 /**
- * Entidade responsável por armazenar a grade de um usuário.
+ * Entidade responsável por armazenar a grade de um usuário
+ * e realizar operações sobre ela (alocar, desalocar, mover...).
  */
 @Entity
 public class Grade extends Model {
@@ -25,29 +25,24 @@ public class Grade extends Model {
 	@Id
 	private Long id;
 	
-	private int periodoCursando = 1;
+	private int periodoCursando;
 	
 	private TipoDeGrade tipoDeGrade = TipoDeGrade.FLUXOGRAMA_OFICIAL;
-	
-	// CREATOR: Grade contem uma lista com as disciplinas
-	// Lista com todas as disciplinas da grade
+
 	@Transient
 	private List<Disciplina> disciplinas;
 
-	// CREATOR: Grade contem uma lista de períodos
-	// Lista com todos os periodos da grade
 	@ManyToMany(cascade=CascadeType.ALL)
 	private List<Periodo> periodos;
 
 	/**
-	 * Construtor para o Ebean
-	 * Inicializa as variaveis e reseta o sistema (aloca primeiro periodo)
-	 * @throws InvalidOperationException 
+	 * Construtor genérico
+	 * Inicializa as variáveis e reseta o sistema (aloca primeiro periodo)
 	 */
 	public Grade() {
 		setPeriodos(new ArrayList<Periodo>());
-		this.disciplinas = new ArrayList<Disciplina>();
-		periodoCursando = 1;
+		setDisciplinas(new ArrayList<Disciplina>());
+		setPeriodoCursando(1);
 
 		for (int i = 0; i < MAXIMO_DE_PERIODOS; i++) {
 			periodos.add(new Periodo());
@@ -58,144 +53,16 @@ public class Grade extends Model {
 	 * Carrega as disciplinas para a grade de acordo com seu tipo.
 	 */
 	public void carregarDisciplinas() {
-		this.disciplinas = CarregadorDeDisciplinas.carregaDisciplinas(tipoDeGrade);
-	}
-
-	/**
-	 * Gera um plano (grade) aleatório.
-	 */
-	public void gerarPlanoAleatorio() {
-		List<Periodo> periodos = new ArrayList<Periodo>();
-		for (int i = 0; i < MAXIMO_DE_PERIODOS; i++) {
-			periodos.add(new Periodo());
-		}
-		this.periodos = periodos;
-		for (Disciplina disciplina : disciplinas) {
-			boolean adicionado = false;
-			do {
-				int id = (Math.abs((new Random()).nextInt()) % MAXIMO_DE_PERIODOS) + 1;
-				try {
-					getPeriodo(id).alocarDisciplina(disciplina, false);
-					adicionado = true;
-				} catch (InvalidOperationException e) {
-				}
-			} while (!adicionado);
-		}
-	}
-
-	/**
-	 * Retorna Disciplina com id passado como argumento, ou null caso nao possua
-	 * disciplina com o dado id.
-	 * 
-	 * @param id
-	 *            da disciplina a ser retornada
-	 * @return Disciplina cujo ID e o passado como argumento
-	 * @throws InvalidOperationException se a disciplina nao existe
-	 */
-	// INFORMATION EXPERT: Grade contem todas as disciplinas
-	public Disciplina getDisciplinaPorID(int id) throws InvalidOperationException {
-		Disciplina retorno = null;
-
-		for (Disciplina i : disciplinas) {
-			if (id == i.getId()) {
-				retorno = i;
-				break;
-			}
-		}
-		
-		if (retorno == null) {
-			throw new InvalidOperationException("Você não pode alocar disciplinas que não existem.");
-		}
-		
-		return retorno;
+		setDisciplinas(CarregadorDeDisciplinas.carregaDisciplinas(tipoDeGrade));
 	}
 	
 	/**
-	 * Pega o periodo pelo numero dele
+	 * Tenta associar uma disciplina a um período.
 	 * 
-	 * @param index 
-	 * 			numero do periodo (1..12)
-	 * @return Periodo desejado
+	 * @param disciplina Disciplina a ser associada.
+	 * @param periodo Período a ser associado.
+	 * @throws InvalidOperationException Caso a associação não seja válida.
 	 */
-	// INFORMATION EXPERT: Grade contem todos os periodos
-	public Periodo getPeriodo(int index) {
-		return getPeriodos().get(index-1);
-	}
-
-	/**
-	 * Retorna List com todas disciplinas presentes na grade do curso
-	 * 
-	 * @return Lista das disciplinas presentes na grade do curso
-	 */
-	// INFORMATION EXPERT: Grade contem uma lista com todas as disciplinas
-	public List<Disciplina> getDisciplinas() {
-		return disciplinas;
-	}
-	
-	/**
-	 * Altera a lista de disciplinas
-	 * 
-	 * @param disciplinas Nova lista
-	 */
-	public void setDisciplinas(List<Disciplina> disciplinas) {
-		this.disciplinas = disciplinas;
-	}
-	
-	/**
-	 * Verifica se a disciplina esta alocada.
-	 * 
-	 * @param disciplina Disciplina que se quer saber se esta alocada
-	 * @return se a disciplina esta alocada ou nao
-	 */
-	// INFORMATION EXPERT: Grade contem todas as disciplinas e periodos
-	private boolean estaAlocado(Disciplina disciplina) {
-		return getPeriodoDaDisciplina(disciplina) != 0;
-	}
-
-	/**
-	 * Verifica qual e o ultimo periodo com alguma disciplina alocada
-	 * 
-	 * @return indice do ultimo periodo com alguma disciplina alocada
-	 */
-	private int obterUltimoPeriodo() {
-		int ultimoPeriodo = 1;
-		for (int i = 1; i <= MAXIMO_DE_PERIODOS; i++) {
-			Periodo p = getPeriodo(i);
-			if (p.totalDeCreditos() > 0) {
-				ultimoPeriodo = i;
-			}
-		}
-		return ultimoPeriodo;
-	}
-
-	/**
-	 * Informa qual e o periodo da disciplina
-	 * @param disciplina 
-	 * 			que se quer saber o periodo
-	 * @return o indice do periodo que contem a disciplina
-	 */
-	// INFORMATION EXPERT: Grade contem todos os periodos
-	public int getPeriodoDaDisciplina(Disciplina disciplina) {
-		int ultimoPeriodo = obterUltimoPeriodo();
-		for (int i = 1; i <= ultimoPeriodo; i++) {
-			if (getPeriodo(i).contains(disciplina)) {
-				return i;
-			}
-		}
-		return 0;
-	}
-	
-	/**
-	 * Tenta associar uma disciplina a um periodo
-	 * 
-	 * @param disciplina 
-	 * 			a ser associada
-	 * @param periodo 
-	 * 			a ser associado
-	 * @return se foi possivel alocar ou nao
-	 * @throws InvalidOperationException caso nao faca sentido alocar
-	 */
-	// INFORMATION EXPERT: Grade contem todas as disciplinas e periodos
 	public void associarDisciplinaAoPeriodo(Disciplina disciplina, int periodo) throws InvalidOperationException {
 		if (periodo < 1 || periodo > 12) {
 			throw new InvalidOperationException("Não podem ser alocadas disciplinas para períodos que não existem.");
@@ -215,7 +82,7 @@ public class Grade extends Model {
 		
 		else {
 			if (preRequisitosFaltando(disciplina, periodo).size() > 0) {
-				throw new InvalidOperationException("Essa disciplina tem pre-requisitos faltando.");
+				throw new InvalidOperationException("Essa disciplina tem pré-requisitos faltando.");
 			}
 			
 			Periodo p = getPeriodo(periodo);
@@ -230,11 +97,11 @@ public class Grade extends Model {
 	}
 	
 	/**
-	 * Move a disciplina para um outro periodo
+	 * Move a disciplina para um outro periodo.
 	 *
-	 * @param disciplina que se quer mover
-	 * @param indexPeriodoNovo periodo para onde se quer mover a disciplina
-	 * @throws InvalidOperationException caso nao possa mover a disciplina para o periodo especificado
+	 * @param disciplina Disciplina que se quer mover.
+	 * @param indexPeriodoNovo Número (1..12) do período para onde se quer mover a disciplina.
+	 * @throws InvalidOperationException Caso não seja válido mover para o período especificado.
 	 */
 	private void moverDisciplina(Disciplina disciplina, int indexPeriodoNovo) throws InvalidOperationException {
 		int indexPeriodoAntigo = getPeriodoDaDisciplina(disciplina);
@@ -254,13 +121,11 @@ public class Grade extends Model {
 	}
 	
 	/**
-	 * Tenta desalocar uma disciplina. Se tiver pos-requisitos alocados, nao permite.
+	 * Tenta desalocar uma disciplina. Se tiver pós-requisitos alocados, não permite.
 	 * 
-	 * @param disciplina 
-	 * 			que se quer desalocar
-	 * @throws InvalidOperationException caso nao faca sentido desalocar (ja desalocada, ou primeiro periodo)
+	 * @param disciplina Disciplina que se quer desalocar.
+	 * @throws InvalidOperationException Caso não seja válido desalocar (já desalocada).
 	 */
-	// INFORMATION EXPERT: Grade contem todos os periodos
 	public void desalocarDisciplina(Disciplina disciplina) throws InvalidOperationException {
 		int indexPeriodo = getPeriodoDaDisciplina(disciplina);
 		
@@ -282,91 +147,36 @@ public class Grade extends Model {
 	}
 	
 	/**
-	 * Verifica todas os pos-requisitos diretos e indiretos, da disciplina, que estao alocados
-	 * 
-	 * @param disciplina 
-	 * 			que se quer saber que pos-requisitos diretos e indiretos estao alocados
-	 * @return list com todas os pos-requisitos diretos e indiretos da disciplina que estao alocados
+	 * Gera um plano (grade) aleatório.
 	 */
-	// INFORMATION EXPERT: Grade contem todas as disciplinas
-	public List<Disciplina> posRequisitosAlocados(Disciplina disciplina) {
-		List<Disciplina> posRequisitosAlocados = new ArrayList<Disciplina>();
-		
-		for (Disciplina i : disciplina.getPosRequisitos()) {
-			if (getPeriodoDaDisciplina(i) != 0 && !posRequisitosAlocados.contains(i)) {
-				for (Disciplina j : posRequisitosAlocados(i)) {
-					if (!posRequisitosAlocados.contains(j)) {
-						posRequisitosAlocados.add(j);
-					}
+	public void gerarPlanoAleatorio() {
+		List<Periodo> periodos = new ArrayList<Periodo>();
+		for (int i = 0; i < MAXIMO_DE_PERIODOS; i++) {
+			periodos.add(new Periodo());
+		}
+		this.periodos = periodos;
+		for (Disciplina disciplina : disciplinas) {
+			boolean adicionado = false;
+			do {
+				int id = (Math.abs((new Random()).nextInt()) % MAXIMO_DE_PERIODOS) + 1;
+				try {
+					getPeriodo(id).alocarDisciplina(disciplina, false);
+					adicionado = true;
+				} catch (InvalidOperationException e) {
 				}
-				posRequisitosAlocados.add(i);
-			}
+			} while (!adicionado);
 		}
-		
-		return posRequisitosAlocados;
+		setPeriodoCursando(obterUltimoPeriodo());
 	}
 	
 	/**
-	 * Verifica que pre-requisitos diretos da disciplina nao estao sendo respeitados
-	 * 
-	 * @param disciplina 
-	 * 			que se quer saber que pre-requisitos diretos nao estao sendo respeitados
-	 * @return list com todos os pre-requisitos diretos que nao estao sendo respeitados
+	 * Reseta a grade de disciplinas de acordo com a lista de disciplinas.
 	 */
-	// INFORMATION EXPERT: Grade contem todas as disciplinas e periodos
-	public List<Disciplina> preRequisitosFaltando(Disciplina disciplina, int periodo) {
-		List<Disciplina> preRequisitosFaltando = new ArrayList<Disciplina>();
-		for (Disciplina i : disciplina.getPreRequisitos()) {
-			if (periodo <= getPeriodoDaDisciplina(i) || !estaAlocado(i)) {
-				preRequisitosFaltando.add(i);
-			}
-		}
-		return preRequisitosFaltando;
-	}
-
-	/**
-	 * Converte a informacao da grade em uma string
-	 * @return String com a descricao do que tem na grade
-	 */
-	// INFORMATION EXPERT: Grade contem todas as disciplinas e periodos
-	public String toString() {
-        String result = "[";
-        List<Disciplina> disciplinasIrregulares = obterDisciplinasIrregulares();
-        for (int i = 0; i < disciplinas.size(); i++) {
-            if (i > 0) {
-                result += ", ";
-            }
-            Disciplina disciplina = disciplinas.get(i);
-            result += "[" + disciplina.toString();
-            result += ", " + getPeriodoDaDisciplina(disciplina);
-            result += ", " + (disciplinasIrregulares.contains(disciplina) ? "1" : "0") + "]";
-        }
-        result += "]";
-        return result;
-	}
-	
-	/**
-	 * Reseta a grade
-	 */
-	// INFORMATION EXPERT: Grade contem todos os periodos
 	public void resetar() {
-		limpar();
-		setar();
-	}
-	
-	/**
-	 * Limpa a grade
-	 */
-	private void limpar() {
 		for (Periodo periodo : getPeriodos()) {
 			periodo.resetar();
 		}
-	}
-	
-	/**
-	 * Aloca as disciplinas em seus periodos previstos, as optativas vao para o ultimo periodo com alguma disciplina
-	 */
-	private void setar() {
+
 		for (Disciplina disciplina : disciplinas) {
 			if (disciplina.getPeriodoPrevisto() == 0) {
 				try {
@@ -386,9 +196,36 @@ public class Grade extends Model {
 	}
 	
 	/**
+	 * Verifica se a disciplina está alocada.
+	 * 
+	 * @param disciplina Disciplina que se quer saber se está alocada.
+	 * @return Se a disciplina esta alocada ou não.
+	 */
+	private boolean estaAlocado(Disciplina disciplina) {
+		return getPeriodoDaDisciplina(disciplina) != 0;
+	}
+
+	/**
+	 * Obtém o número do último periodo com alguma disciplina alocada.
+	 * Em caso de não haver disciplina alocada, considera-se que é o período 1.
+	 * 
+	 * @return Número (1..12) do último periodo com alguma disciplina alocada.
+	 */
+	private int obterUltimoPeriodo() {
+		int ultimoPeriodo = 1;
+		for (int i = 1; i <= MAXIMO_DE_PERIODOS; i++) {
+			Periodo p = getPeriodo(i);
+			if (p.totalDeCreditos() > 0) {
+				ultimoPeriodo = i;
+			}
+		}
+		return ultimoPeriodo;
+	}
+
+	/**
 	 * Obtém uma lista das disciplinas irregulares na grade.
 	 * Uma disciplina está irregular se está alocada sem que todos os seus
-	 * pré-requisitos estejam alocadas em períodos inferiores ao dela.
+	 * pré-requisitos estejam alocados em períodos inferiores ao dela.
 	 * 
 	 * @return Lista das disciplinas irregulares.
 	 */
@@ -419,64 +256,211 @@ public class Grade extends Model {
 	}
 
 	/**
-	 * Retorna o id unico da grade
+	 * Obtém disciplina com id passado como argumento.
 	 * 
-	 * @return id da grade
+	 * @param id Identificador da disciplina a ser obtida.
+	 * @return Disciplina cujo ID e o passado como argumento
+	 * @throws InvalidOperationException Se a disciplina não existe.
+	 */
+	public Disciplina getDisciplinaPorID(int id) throws InvalidOperationException {
+		Disciplina disciplina = null;
+
+		for (Disciplina d : disciplinas) {
+			if (id == d.getId()) {
+				disciplina = d;
+				break;
+			}
+		}
+		
+		if (disciplina == null) {
+			throw new InvalidOperationException("Você não pode alocar disciplinas que não existem.");
+		}
+		
+		return disciplina;
+	}
+
+	/**
+	 * Informa qual é o período da disciplina.
+	 *
+	 * @param disciplina Disciplina que se quer saber o período.
+	 * @return O número (0*..12) do período da disciplina.
+	 * 			*Em caso de não estar em nenhum, considera-se que está no 0.
+	 */
+	public int getPeriodoDaDisciplina(Disciplina disciplina) {
+		int ultimoPeriodo = obterUltimoPeriodo();
+		for (int i = 1; i <= ultimoPeriodo; i++) {
+			if (getPeriodo(i).contains(disciplina)) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	/**
+	 * Obtém todos os pós-requisitos (diretos e indiretos) da disciplina que estão alocados.
+	 * 
+	 * @param disciplina Disciplina da qual se quer obter os pós-requisitos alocados.
+	 * @return Lista com os pós-requisitos alocados.
+	 */
+	public List<Disciplina> posRequisitosAlocados(Disciplina disciplina) {
+		List<Disciplina> posRequisitosAlocados = new ArrayList<Disciplina>();
+		
+		for (Disciplina i : disciplina.getPosRequisitos()) {
+			if (getPeriodoDaDisciplina(i) != 0 && !posRequisitosAlocados.contains(i)) {
+				for (Disciplina j : posRequisitosAlocados(i)) {
+					if (!posRequisitosAlocados.contains(j)) {
+						posRequisitosAlocados.add(j);
+					}
+				}
+				posRequisitosAlocados.add(i);
+			}
+		}
+		
+		return posRequisitosAlocados;
+	}
+	
+	/**
+	 * Obtém todos os pré-requisitos (diretos) da disciplina que não estão alocados.
+	 * Disciplinas alocadas em período posterior ou igual a periodoDaDisciplina são
+	 * consideradas não alocadas.
+	 * 
+	 * @param disciplina Disciplina da qual se quer os pré-requisitos não alocados.
+	 * @param periodoDaDisciplina Período a partir do qual as disciplinas são
+	 * 			automaticamente consideradas desalocadas.
+	 * @return Lista com os pré-requisitos não alocados.
+	 */
+	public List<Disciplina> preRequisitosFaltando(Disciplina disciplina, int periodoDaDisciplina) {
+		List<Disciplina> preRequisitosFaltando = new ArrayList<Disciplina>();
+		for (Disciplina i : disciplina.getPreRequisitos()) {
+			if (periodoDaDisciplina <= getPeriodoDaDisciplina(i) || !estaAlocado(i)) {
+				preRequisitosFaltando.add(i);
+			}
+		}
+		return preRequisitosFaltando;
+	}
+
+	/**
+	 * Obtém o período pelo número (ordenado).
+	 * 
+	 * @param index Número do periodo (1..12)
+	 * @return Objeto do período desejado.
+	 */
+	public Periodo getPeriodo(int index) {
+		return getPeriodos().get(index-1);
+	}
+
+	/**
+	 * Obtém uma lista com todas as disciplinas presentes nesta grade.
+	 * 
+	 * @return Lista das disciplinas presentes nesta grade.
+	 */
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+	
+	/**
+	 * Altera a lista de disciplinas.
+	 * 
+	 * @param disciplinas Nova lista de disciplinas.
+	 */
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+
+	/**
+	 * Retorna o id da grade.
+	 * 
+	 * @return Identificador numérico da grade.
 	 */
 	public Long getId() {
 		return id;
 	}
 
 	/**
-	 * Atribui um id ao periodo
+	 * Atribui um id à grade.
 	 *
-	 * @param id atribuido
+	 * @param Identificador a ser atribuído.
 	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
 	/**
-	 * Retorna o periodo que esta sendo cursado
+	 * Obtém o periodo que está sendo cursado.
 	 * 
-	 * @return inteiro correspondente ao periodo que o usuario esta cursando
+	 * @return Período sendo cursado.
 	 */
 	public int getPeriodoCursando() {
 		return periodoCursando;
 	}
 
 	/**
-	 * Atribui um valor ao periodo cursando
+	 * Altera o período que está sendo cursado.
 	 * 
-	 * @param periodoCursando valor a ser atribuido
+	 * @param periodoCursando Período que está sendo cursado.
 	 */
 	public void setPeriodoCursando(int periodoCursando) {
 		this.periodoCursando = periodoCursando;
 	}
 
 	/**
-	 * Retorna lista com os periodos
+	 * Obtém lista com os períodos da grade.
 	 * 
-	 * @return lista com os periodos
+	 * @return Lista com os períodos.
 	 */
 	public List<Periodo> getPeriodos() {
 		return periodos;
 	}
 
 	/**
-	 * Atribui uma lista de periodos a grade
+	 * Atribui uma lista de periodos à grade.
 	 * 
-	 * @param periodos lista a ser atribuida a grade
+	 * @param periodos Lista de períodos a ser atribuída.
 	 */
 	public void setPeriodos(List<Periodo> periodos) {
 		this.periodos = periodos;
 	}
 
+	/**
+	 * Obtém o tipo da grade.
+	 * 
+	 * @return Tipo da grade.
+	 */
 	public TipoDeGrade getTipoDeGrade() {
 		return tipoDeGrade;
 	}
 
+	/**
+	 * Atribui um novo tipo à grade; se for diferente do anterior,
+	 * atualiza a lista de disciplinas.
+	 * 
+	 * @param tipoDeGrade Novo tipo da grade.
+	 */
 	public void setTipoDeGrade(TipoDeGrade tipoDeGrade) {
-		this.tipoDeGrade = tipoDeGrade;
+		if (this.tipoDeGrade != tipoDeGrade) {
+			this.tipoDeGrade = tipoDeGrade;
+			carregarDisciplinas();
+		}
+	}
+
+	/**
+	 * Converte a informação da grade em uma string.
+	 * @return String com a descrição do que tem na grade.
+	 */
+	@Override
+	public String toString() {
+        String result = "[";
+        List<Disciplina> disciplinasIrregulares = obterDisciplinasIrregulares();
+        for (int i = 0; i < disciplinas.size(); i++) {
+            if (i > 0) {
+                result += ", ";
+            }
+            Disciplina disciplina = disciplinas.get(i);
+            result += "[" + disciplina.toString();
+            result += ", " + getPeriodoDaDisciplina(disciplina);
+            result += ", " + (disciplinasIrregulares.contains(disciplina) ? "1" : "0") + "]";
+        }
+        result += "]";
+        return result;
 	}
 }
