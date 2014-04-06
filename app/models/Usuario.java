05cfded7de7d424a1de7fd8cd8b62191e56d5a93
rafaelclp <rafael.clp@hotmail.com>
2014-04-06 16:24:49 -0300

    Refatorando classe Usuario, já que grande parte de suas tarefas foram para CadastroDeUsuario

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -1,11 +1,5 @@
 package models;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -14,23 +8,13 @@ import javax.persistence.OneToOne;
 import play.db.ebean.Model;
 
 /**
- * 
- * Classe que representa o aluno
- *
+ * Entidade responsável por armazenar as informações de um usuário,
+ * incluindo nome (real e de usuário), senha (hasheada), salt e grade.
  */
 @Entity
 public class Usuario extends Model {
 	private static final long serialVersionUID = 8941221341321L;
 
-	private static final int TAMANHO_USUARIO_MINIMO = 3;
-	private static final int TAMANHO_USUARIO_MAXIMO = 20;
-
-	private static final int TAMANHO_NOME_MINIMO = 3;
-	private static final int TAMANHO_NOME_MAXIMO = 64;
-
-	private static final int TAMANHO_SENHA_MINIMO = 6;
-	private static final int TAMANHO_SENHA_MAXIMO = 64;
-	
 	@Id
 	private Long id;
 
@@ -45,266 +29,127 @@ public class Usuario extends Model {
 	@OneToOne(cascade=CascadeType.ALL)
 	private Grade grade;
 	
-	private static Finder<String, Usuario> find = new Finder<String, Usuario>(String.class,
-			Usuario.class);
-	
 	/**
-	 * Constructor
+	 * Construtor de novo usuário
 	 * 
-	 * @param nome do aluno
-	 * @param usuario do aluno
-	 * @param senha do aluno
+	 * @param nome Nome do aluno
+	 * @param usuario Nome de usuário do aluno
+	 * @param senhaHasheada Senha, já hasheada, do aluno
+	 * @param salt Salt usado ao hashear a senha
 	 */
-	public Usuario(String nome, String usuario, String senha) {
+	public Usuario(String nome, String usuario, String senhaHasheada, String salt) {
 		setNome(nome);
 		setUsuario(usuario);
-		setSenha(senha);
+		setSenhaHasheada(senhaHasheada);
+		setSalt(salt);
 		setGrade(new Grade());
 	}
 
 	/**
-	 * Gera o salt
+	 * Obtém o nome real do usuário.
 	 * 
-	 * @return salt gerado
-	 */
-	private String gerarSalt() {
-		return (new BigInteger(64, new SecureRandom())).toString(32);
-	}
-
-	/**
-	 * Retorna o nome do aluno
-	 * 
-	 * @return nome do aluno
+	 * @return Nome real do usuário.
 	 */
 	public String getNome() {
 		return nome;
 	}
 
 	/**
-	 * Atribui um nome ao aluno
+	 * Atribui um nome real ao usuário.
 	 * 
-	 * @param nome atribuido
+	 * @param nome Nome atribuído.
 	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
 	/**
-	 * Retorna o usuario do aluno
+	 * Obtém o nome de usuário deste usuário.
 	 * 
-	 * @return usuario do aluno
+	 * @return Nome de usuário.
 	 */
 	public String getUsuario() {
 		return usuario;
 	}
 
 	/**
-	 * Atribui um usuario ao aluno
+	 * Atribui um nome de usuário.
 	 * 
-	 * @param usuario atribuido
+	 * @param usuario Usuário atribuido.
 	 */
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
 
 	/**
-	 * Retorna o id do aluno
+	 * Obtém o id do usuário
 	 * 
-	 * @return id do aluno
+	 * @return Id do aluno
 	 */
 	public Long getId() {
 		return id;
 	}
 
 	/**
-	 * Atribui um id ao aluno
+	 * Atribui um id ao usuário
 	 * 
-	 * @param id atribuido
+	 * @param Id atribuído
 	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
 
 	/**
-	 * Retorna o salt
+	 * Obtém o salt usado para hashear a senha deste usuário.
 	 * 
-	 * @return salt
+	 * @return Salt do usuário.
 	 */
 	public String getSalt() {
 		return salt;
 	}
 
 	/**
-	 * Atribui um salt
+	 * Atribui um salt.
 	 * 
-	 * @param salt atribuido
+	 * @param salt Salt a ser atribuído.
 	 */
 	public void setSalt(String salt) {
 		this.salt = salt;
 	}
 
 	/**
-	 * Retorna a senha do aluno apos passar pelo hash
+	 * Obtém a senha (hasehada) do usuário.
 	 * 
-	 * @return senha hasheada
+	 * @return Senha hasheada
 	 */
 	public String getSenhaHasheada() {
 		return senhaHasheada;
 	}
 
 	/**
-	 * Atribui um valor a senha hasheada do aluno
+	 * Altera a senha (hasheada) do usuário.
 	 * 
-	 * @param senhaHasheada atribuida
+	 * @param senhaHasheada Senha a ser atribuída
 	 */
 	public void setSenhaHasheada(String senhaHasheada) {
 		this.senhaHasheada = senhaHasheada;
 	}
-	
-	/**
-	 * Faz o hash da senha do aluno
-	 * 
-	 * @param senha do aluno
-	 * @param salt
-	 * @return senha hasheada
-	 */
-	private static String hashearSenha(String senha, String salt) {
-		String senhaComSalt = (senha+salt);
-		String senhaHasheada = null;
-		try {
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			md.update(senhaComSalt.getBytes(), 0, senhaComSalt.length());
-			senhaHasheada = new BigInteger(1, md.digest()).toString(16);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-
-		return senhaHasheada;
-	}
 
 	/**
-	 * Atribui um valor a senha do aluno
+	 * Retorna a grade do usuário.
 	 * 
-	 * @param senha valor atribuido
-	 */
-	public void setSenha(String senha) {
-		setSalt(gerarSalt());
-		setSenhaHasheada(hashearSenha(senha, salt));
-	}
-
-	/**
-	 * Retorna a grade do aluno
-	 * 
-	 * @return grade do aluno
+	 * @return Grade do usuário.
 	 */
 	public Grade getGrade() {
 		return grade;
 	}
 
 	/**
-	 * Atribui uma grade ao aluno
+	 * Atribui uma grade ao usuário.
 	 * 
-	 * @param grade atribuida
+	 * @param grade Grade atribuída.
 	 */
 	public void setGrade(Grade grade) {
 		this.grade = grade;
-	}
-	
-	/**
-	 * Verifica se o usuario ja existe
-	 * 
-	 * @param usuario que se quer verificar se existe
-	 * @return se o usuario existe
-	 */
-	public static boolean existeUsuario(String usuario) {
-		List<Usuario> listaUsuarios = find
-				.where()
-					.eq("usuario", usuario)
-				.findList();
-
-		return listaUsuarios.size() >= 1;
-	}
-
-	/**
-	 * Verifica se o usuario e valido
-	 * 
-	 * @param usuario que se quer verificar se e valido
-	 * @return se o usuario e valido
-	 */
-	private static boolean ehUsuarioValido(String usuario) {
-		return usuario.length() >= TAMANHO_USUARIO_MINIMO && usuario.length() <= TAMANHO_USUARIO_MAXIMO;
-	}
-
-	/**
-	 * Verifica se o nome e valido
-	 * 
-	 * @param nome que se quer verificar se e valido
-	 * @return se o nome e valido
-	 */
-	private static boolean ehNomeValido(String nome) {
-		return nome.length() >= TAMANHO_NOME_MINIMO && nome.length() <= TAMANHO_NOME_MAXIMO;
-	}
-
-	/**
-	 * Verifica se a senha e valida
-	 * 
-	 * @param senha que se quer verificar se e valida
-	 * @return se a senha e valida
-	 */
-	private static boolean ehSenhaValida(String senha) {
-		return senha.length() >= TAMANHO_SENHA_MINIMO && senha.length() <= TAMANHO_SENHA_MAXIMO;
-	}
-
-	/**
-	 * Cria o usuario
-	 * 
-	 * @param nome do aluno
-	 * @param usuario do aluno
-	 * @param senha do aluno
-	 * @throws InvalidOperationException se nao puder criar o usuario
-	 */
-	public static Usuario registrar(String nome, String usuario, String senha) throws InvalidOperationException {
-		if (!ehUsuarioValido(usuario)) {
-			throw new InvalidOperationException("O usuário é inválido.");
-		}
-		if (!ehNomeValido(nome)) {
-			throw new InvalidOperationException("O nome é inválido.");
-		}
-		if (!ehSenhaValida(senha)) {
-			throw new InvalidOperationException("A senha é inválida.");
-		}
-		if (existeUsuario(usuario)) {
-			throw new InvalidOperationException("O usuário informado já está cadastrado.");
-		}
-
-		Usuario u = new Usuario(nome, usuario, senha);
-		u.save();
-		return u;
-	}
-	
-	/**
-	 * Loga o usuario
-	 * 
-	 * @param usuario do aluno
-	 * @param senha do aluno
-	 * @return o usuario que foi logado
-	 * @throws InvalidOperationException caso nao possa logar o usuario
-	 */
-	public static Usuario logar(String usuario, String senha) throws InvalidOperationException {
-		Usuario u = null;
-		List<Usuario> listaUsuarios = find
-				.where()
-					.eq("usuario", usuario)
-				.findList();
-
-		if (listaUsuarios.size() >= 1) {
-			u = listaUsuarios.get(0);
-			if (!u.getSenhaHasheada().equals(hashearSenha(senha, u.getSalt()))) {
-				throw new InvalidOperationException("A senha está incorreta.");
-			}
-		} else {
-			throw new InvalidOperationException("O usuário é inexistente.");
-		}
-		return u;
 	}
 }
