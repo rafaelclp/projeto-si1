e661f53593edf4a4d7711d7ef065f7d71a4e199e
rafaelclp <rafael.clp@hotmail.com>
2014-04-07 11:11:47 -0300

    Adicionado o javadoc

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -10,6 +10,14 @@ import play.mvc.Security;
  */
 public class Secured extends Security.Authenticator {
 
+	/**
+	 * Realiza algumas checagens de usuário. Caso não falhe
+	 * em nenhuma, retorna o nome de usuário logado.
+	 * 
+	 * @param ctx Contexto que contém a sessão.
+	 * 
+	 * @return Nome de usuário logado, ou null.
+	 */
     @Override
     public String getUsername(Context ctx) {
     	CadastroDeUsuario cadastroDeUsuario = new CadastroDeUsuario();
@@ -26,6 +34,14 @@ public class Secured extends Security.Authenticator {
     	return usuario;
     }
 
+    /**
+     * Em caso de não estar autorizado, retorna um redirecionamento
+     * para a página de login (indexLogin).
+     * 
+     * @param ctx Contexto que contém a sessão [ignorado].
+     * 
+     * @return Página com o redirecionamento.
+     */
     @Override
     public Result onUnauthorized(Context ctx) {
         return redirect(routes.Application.indexLogin());
